efce2352af9e0ed74b178886bde2bc3a7fa01f3a
Hasan Jafer <hasanjafer2022@gmail.com>
2024-03-19 14:01:13 +0530

    video-68 added
    JavaScript - Selecting by Ids, Classes, and More

---
+++ video-68/script.js
@@ -0,0 +1,29 @@
+console.log("Hasan jafer")
+
+let bupendraJogis = document.getElementsByClassName("box")
+console.log(bupendraJogis)
+
+// bupendraJogis[2].style.backgroundColor = "red"
+
+// document.getElementById("red-box").style.backgroundColor="red"
+
+// document.querySelector(".box").style.backgroundColor = "green"
+// document.querySelectorAll(".box").forEach(e =>{
+//     e.style.backgroundColor = "green"
+// })
+
+// e = document.getElementsByTagName("div")
+
+// e[3].matches("#red-box")
+
+// false this will return false
+
+// e[4].matches("#red-box")
+
+// this will return true
+
+// e[4].closest("html")
+
+// document.querySelector(".container").contains(e[2])
+
+// document.querySelector("body").contains(document.querySelector(".container"))
