64d30d6e07587e224708d0a6786d33111579d089
Hasan Jafer <hasanjafer2022@gmail.com>
2024-03-20 13:19:53 +0530

    video-71 added
    Inserting and Removing Elements using JavaScript

---
+++ video-71/script.js
@@ -0,0 +1,23 @@
+// let div = document.createElement("div");
+//         div.innerHTML = " I have been inserted <b>  by hasan </b>"
+//         div.setAttribute("class", "created");
+        // document.querySelector(".container").append(div);
+        // document.querySelector(".container").prepend(div);
+        // document.querySelector(".container").before(div);
+        // document.querySelector(".container").after(div);
+
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("beforebegin","<b>I am under the water. please help  to get rid out of it</b>")
+
+// document.querySelector(".box").remove()
+
+document.querySelector(".container").classList
+
+
+document.querySelector(".container").className
+
+document.querySelector(".container").classList.add("hasan")
+
+document.querySelector(".container").classList.remove("hasan")
+
+document.querySelector(".container").classList.toggle("hasan")  //means on and off
